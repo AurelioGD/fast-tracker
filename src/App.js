a776a66e64d93f1da6e9c1e17a5bd903ee00b46e
Aurelio Gonzalez <agonzalez@sisu.mx>
2022-12-12 23:32:55 -0600

    Agrega control del estado

--- src/App.js
+++ src/App.js
@@ -1,22 +1,69 @@
-import './App.css';
-import playButtonIcon from "./assets/play-button.svg"
-import stopIcon from "./assets/stop.svg"
-import CardDay from './components/CardDay';
+import { useState } from "react";
+import "./App.css";
+import playButtonIcon from "./assets/play-button.svg";
+import stopIcon from "./assets/stop.svg";
+import CardDay from "./components/CardDay";
 
 function App() {
+  const [formState, setFormState] = useState({
+    task: "",
+    hours: 0,
+    minutes: 0,
+    seconds: 0
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if(name === "task"){
+      setFormState({ ...formState, [name]: value });
+      return
+    }
+    const timeToSet = Number(value)
+
+    if (name === "hours" && (timeToSet > 24 || timeToSet < 0)) return;
+    if (name === "minutes" && (timeToSet > 59|| timeToSet < 0)) return;
+    if (name === "seconds" && (timeToSet > 59|| timeToSet < 0)) return;
+
+    setFormState({ ...formState, [name]: timeToSet }); 
+  };
   return (
     <div className="App">
-      <div className='container-inputs'>
-        <input className='description-bar-input' placeholder='What are you working on?'/>
-        <input className='cicle-input'/>
-        <span className='double-points'>:</span>
-        <input className='cicle-input'/>
-        <span className='double-points'>:</span>
-        <input className='cicle-input'/>
-        <button className='button-action'><img className='img-button' src={stopIcon} alt='icon'/></button>
+      <div className="container-inputs">
+        <input
+          name="task"
+          className="description-bar-input"
+          placeholder="What are you working on?"
+          onChange={handleChange}
+          value={formState.task}
+        />
+        <input
+          name="hours"
+          className="cicle-input"
+          type="Number"
+          onChange={handleChange}
+          value={formState.hours}
+        />
+        <span className="double-points">:</span>
+        <input
+          name="minutes"
+          className="cicle-input"
+          type="Number"
+          onChange={handleChange}
+          value={formState.minutes}
+        />
+        <span className="double-points">:</span>
+        <input
+          name="seconds"
+          className="cicle-input"
+          type="Number"
+          onChange={handleChange}
+          value={formState.seconds}
+        />
+        <button className="button-action">
+          <img className="img-button" src={stopIcon} alt="icon" />
+        </button>
       </div>
-      <div className='container-day-cards'>
-        <CardDay/>
+      <div className="container-day-cards">
+        <CardDay />
       </div>
     </div>
   );
